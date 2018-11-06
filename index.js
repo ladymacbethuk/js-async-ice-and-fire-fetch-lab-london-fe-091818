e6ccb37fdf7b877f73b5917e9e213319165bfcf0
Stephanie Phillips <steph@ladymacbeth.uk>
2018-11-06 18:46:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
-  ('https://anapioficeandfire.com/api/books')
+  fetch('https://anapioficeandfire.com/api/books')
 }
 
 function renderBooks(json) {
